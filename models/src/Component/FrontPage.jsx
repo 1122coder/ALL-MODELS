1ae6c33c4879c727625d6d4f7d60dc6168987399
Aleena <aleenaza90@gmail.com>
2024-02-20 15:48:57 +0500

    updated

--- models/src/Component/FrontPage.jsx
+++ models/src/Component/FrontPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import '../Style/FrontPage.css'; // Make sure to create this CSS file
+
+import '../Style/FrontPage.css';
 
 const FrontPage = () => {
+ 
+
   return (
     <div className="container">
       <h1 className="gradient-text">All models <span className="animated-text">at one place</span></h1>
       <p className="subtitle">The place where you can play with different models</p>
+      <button className="button">
+  Get Started <span className="arrow">→</span>
+</button>
     </div>
   );
-}
+};
 
 export default FrontPage;
